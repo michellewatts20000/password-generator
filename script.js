8940032064991683d9495e45867bebdf551c6466
michellewatts20000 <watts.e.michelle@gmail.com>
2021-03-11 22:22:01 +1100

    generated a prompt and an alert for asking user how many characters they want in their password

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var numbers = window.prompt("How many characters do you want in your password?");
+
+if (numbers < 8) {
+  window.alert("You don't have enough characters");
+} else if 
+  (numbers > 256) {
+    window.alert("You have too many characters");
+  }
+else {
+  var specialCharacters = window.prompt("Do you want any special characters in your password?");
+}
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
