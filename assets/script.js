d2e5491e90218c735fbf682cfa533ea353ec005d
Michelle Watts <watts.e.michelle@gmail.com>
2021-03-12 14:43:56 +1100

    added window.confirm and window.alert

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,16 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var numbers = window.prompt("How many characters do you want in your password?");
 
-if (numbers < 8) {
-  window.alert("You don't have enough characters");
-} else if 
-  (numbers > 256) {
+  if (numbers < 8) {
+    window.alert("You don't have enough characters");
+  } else if (numbers > 128) {
     window.alert("You have too many characters");
+  } else {
+    var uSpecial = window.confirm("Click OK if you want special characters in your password.");
+    var uNumbers = window.confirm("Click OK if you want numbers in your password.");
+    var uLowercase = window.confirm("Click OK if you want lowercase letters in your password.");
+    var uUppercase = window.confirm("Click OK if you want uppercase letters in your password.");
   }
-else {
-  var specialCharacters = window.prompt("Do you want any special characters in your password?");
-}
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
