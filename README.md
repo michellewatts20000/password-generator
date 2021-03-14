0845cf290bb96ca4d5506875aeaa55eb7b03ef04
michellewatts20000 <watts.e.michelle@gmail.com>
2021-03-14 14:53:51 +1100

    added more comments to the script file, added more detail to the readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# password-generator
+# Password Generator - submission by Michelle Watts
+
+## User Story
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Link to the GitHub page is here: https://michellewatts20000.github.io/password-generator/
+## Link to the GitHub repo is here: https://github.com/michellewatts20000/password-generator
+
+<img src="./assets/img/screenshot.png">
--- assets/script.js
+++ assets/script.js
@@ -3,47 +3,50 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // Store the length the user wants the password to be
-  var complexity = document.querySelector(".complexity").value;
+  var numberCharacters = document.querySelector(".numberCharacters").value;
 
-  if (complexity < 8 || complexity > 128) {
+  // alert user that they haven't specified enough characters and return them out of the function so they have to start again
+  if (numberCharacters < 8 || numberCharacters > 128) {
     alert("Please choose a number between 8 and 128.")
     return;
   }
 
-  // Store the values from the checkboxes
-  var useSpecialChars = document.querySelector(".cb_special").checked;
-  var useNumeric = document.querySelector(".cb_numeric").checked;
-  var useLowercase = document.querySelector(".cb_lowercase").checked;
-  var useUppercase = document.querySelector(".cb_uppercase").checked;
+  // Store the values from the checkboxes, if checked
+  var yesSpecialChars = document.querySelector(".user_special").checked;
+  var yesNumbers = document.querySelector(".user_numeric").checked;
+  var yesLowercase = document.querySelector(".user_lowercase").checked;
+  var yesUppercase = document.querySelector(".user_uppercase").checked;
 
-  // if no checkboxes checked show alert
-  if (!useSpecialChars && !useNumeric && !useLowercase && !useUppercase) {
+  // if no checkboxes checked by user show alert
+  if (!yesSpecialChars && !yesNumbers && !yesLowercase && !yesUppercase) {
 
-    alert("Please make at least one selection.")
+    alert("Please make at least one character type selection.")
     return;
   }
 
-  // Requirement values
-  var specialChars = '!@#$%^&*()_+';
-  var numericChars = '1234567890';
+ // library of all the options
+  var specialChars = '!@#$%^&*()<>={}[],.?~`/;:"-_+';
+  var numberChars = '0123456789';
   var lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
   var upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-  // Build out the final value string in which to build the password from.
-  let values = `${useUppercase ? upperCaseChars : ''}${useLowercase ? lowerCaseChars : ''}${useSpecialChars ? specialChars: ''}${useNumeric ? numericChars : ''}`;
+  // create unique values string based on what checkboxes were checked, found this solution here: https://github.com/zsadov1/Password-Generator-3/blob/master/JS/script.js
+  let userChoice = `${yesUppercase ? upperCaseChars : ''}${yesLowercase ? lowerCaseChars : ''}${yesSpecialChars ? specialChars: ''}${yesNumbers ? numberChars : ''}`;
 
+  // an empty string to store the new password
   var password = '';
 
   // Create for loop to choose password characters
-  for (var i = 0; i < complexity; i++) {
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  for (var i = 0; i < numberCharacters; i++) {
+    password = password + userChoice.charAt(Math.floor(Math.random() * Math.floor(userChoice.length - 1)));
   }
 
+  // displays the password in the textfield looks for the class "showit" in the html
   document.querySelector(".showit").value = password;
-
 }
 
 
+// function triggered by button click - to copy password
 function copyPassword() {
   var password = document.querySelector(".showit");
   password.select();
